1e7f7909217c88b7bc7414df9f1cbfcba8a79984
najandhamedua <hamed.najand@alumni.univ-avignon.fr>
2021-11-10 18:42:36 +0100

    Implementation of getResult for 2

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,8 @@
 public class FizzBuzz {
     public String getResult(int i) {
-        return "1";
+
+        if(i == 1)
+            return "1";
+        return "2";
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -8,4 +8,10 @@ public class FizzBuzzTest {
         FizzBuzz fb = new FizzBuzz();
         assertEquals("1", fb.getResult(1));
     }
+
+    @Test
+    void shouldReturn2When2() {
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals("2", fb.getResult(2));
+    }
 }
